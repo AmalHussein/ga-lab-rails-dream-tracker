3e836e0ea5b16c4f7fd433971a2df11ad15638bd
Amal Hussein <amalhussein08@gmail.com>
2013-10-15 16:58:44 -0400

    Added a new column

--- dream_tracker/db/schema.rb
+++ dream_tracker/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131015200611) do
+ActiveRecord::Schema.define(version: 20131015205626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20131015200611) do
     t.integer  "happy"
     t.text     "storyline"
     t.string   "characters"
+    t.string   "title"
   end
 
 end
