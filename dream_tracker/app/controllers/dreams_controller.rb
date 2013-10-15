8c68c93b69375061f91ccc49089220e9a53397ce
Amal Hussein <amalhussein08@gmail.com>
2013-10-15 16:24:35 -0400

    Added methods and another migreatrion

---
+++ dream_tracker/app/controllers/dreams_controller.rb
@@ -0,0 +1,59 @@
+class DreamsController < ApplicationController
+	before_action :set_dream, only: [ :show, :edit, :update, :destroy ]
+	
+	def show
+	end 
+
+	def index
+		@dreams = Dream.all 
+	end 
+
+	def new
+		@dream = Dream.new 
+	end 
+
+	def create
+		@dream = Dream.new(spider_params)
+		if @dream.save 
+			redirect_to @dream
+		else 
+			render action: 'new'
+		end 
+	end 
+
+	def edit
+	end 
+
+	def update 
+		if @dream.update(dream_params)
+			redirect_to @dream
+		else 
+			render action: 'edit'
+		end 
+	end 
+
+	def destroy 
+		@dream.destroy
+		redirect_to dreams_url 
+	end 
+
+private 
+
+def set_dream
+	@dream = Dream.find(params[:id])
+end 
+
+def dream_params
+	params.require(:dream).permit(:date, 
+																	:poisonous, 
+																	:weight, 
+																	:habitat, 
+																	:terror, 
+																	:color, 
+																	:origin,
+																	:image_url)
+end 
+
+end 
+
+
---
+++ dream_tracker/app/models/dream.rb
@@ -0,0 +1,7 @@
+class Dream < ActiveRecord::Base 
+end 
+
+
+
+
+
