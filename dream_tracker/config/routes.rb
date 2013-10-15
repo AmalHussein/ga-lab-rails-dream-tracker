50c519dee9f917b49e5564f70869b3f1c692a3a1
Amal Hussein <amalhussein08@gmail.com>
2013-10-15 16:27:34 -0400

    Added routes

--- dream_tracker/config/routes.rb
+++ dream_tracker/config/routes.rb
@@ -1,4 +1,9 @@
 DreamTracker::Application.routes.draw do
+  
+    root 'spider#index'
+    resources :dreams 
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
